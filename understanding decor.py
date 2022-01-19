9c95291f4659a4318cf69412064d41a8a4fd7fb2
brylimz <superuser@brylimz.com>
2022-01-19 13:37:15 +0700

    deep learning

--- understanding decor.py
+++ understanding decor.py
@@ -81,10 +81,9 @@ def another_stand_alone_function():
 
 another_stand_alone_function()
 
-
 # accumulate decorators
 
-print("acumulate decorators")
+print("------acumulate decorators------")
 
 
 def bread(func):
@@ -92,7 +91,8 @@ def bread(func):
         print("</''''''\>")
         func()
         print("<\''''''/>")
-    return wrapper()
+
+    return wrapper
 
 
 def ingredients(func):
@@ -100,8 +100,103 @@ def ingredients(func):
         print("#tomatoes#")
         func()
         print("~salad~")
-    return wrapper()
+
+    return wrapper
 
 
 def sandwich(food="--ham--"):
     print(food)
+
+
+sandwich()
+sandwich = bread(ingredients(sandwich))
+sandwich()
+
+print("--USING THE DECORATOR SYNTAX")
+
+
+@bread
+@ingredients
+def sandwich(food="--ham--"):
+    print(food)
+
+
+sandwich()
+
+print("---ORDER DECORATORS MATTERS")
+
+
+@ingredients
+@bread
+def strange_sandwich(food="--ham--"):
+    print(food)
+
+
+strange_sandwich()
+# answering the question
+print("------BOLD AND ITALIC ------")
+
+
+def makebold(fn):
+    def wrapper():
+        return "<b>" + fn() + "</b>"
+
+    return wrapper
+
+
+def makeitalic(fn):
+    def wrapper():
+        return "<i>" + fn() + "</i>"
+
+    return wrapper
+
+
+@makebold
+@makeitalic
+def say():
+    return "hello"
+
+
+print(say())
+
+# the next level of decorators
+print("------ THE NEXT LEVEL OF DECORATORS ------")
+
+
+def a_decorator_passing_arguments(function_to_decorate):
+    def a_wrapper_accepting_arguments(arg1, arg2):
+        print("I got args! Look: {0}, {1}".format(arg1, arg2))
+        function_to_decorate(arg1, arg2)
+    return a_wrapper_accepting_arguments
+
+
+@a_decorator_passing_arguments
+def print_full_name(first_name, last_name):
+    print("My name is {0} {1}".format(first_name, last_name))
+
+
+print_full_name("Petter", "VS")
+
+# decorating methods
+print("----- decorating methods -----")
+
+
+def method_friendly_decorator(method_to_decorate):
+    def wrapper(self, lie):
+        lie = lie - 3
+        return method_to_decorate(self, lie)
+    return wrapper
+
+
+class Lucy(object):
+
+    def __init__(self):
+        self.age = 32
+
+    @method_friendly_decorator
+    def say_ur_age(self, lie):
+        print(" I am {0}, what did you think?".format(self.age + lie))
+
+
+I = Lucy()
+I.say_ur_age(-3)
